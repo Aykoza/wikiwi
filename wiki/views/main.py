0fe02d504db43ce7ee3108c9d6757dbe76ad2045
aykoza <zv.am@yandex.ru>
2019-07-24 07:49:51 +0700

    begin ajax requests

--- wiki/views/main.py
+++ wiki/views/main.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-
+from django.http import JsonResponse
 
 class Main(TemplateView):
     template_name = 'index.html'
--- wiki/views/module.py
+++ wiki/views/module.py
@@ -1,6 +1,8 @@
+from django.template.loader import render_to_string
 from django.views import generic
 from wiki.forms import ModuleForm
 from wiki.models import Module
+from django.http import JsonResponse, request, Http404
 
 
 class ModuleList(generic.ListView):
@@ -8,8 +10,16 @@ class ModuleList(generic.ListView):
     template_name = 'error_list.html'
     form = ModuleForm
 
-    def get_context_data(self, **kwargs):
-        context = super(ModuleList, self).get_context_data(**kwargs)
-        context['error_list'] = Module.objects.all()
-        context['form'] = self.form
-        return context
+    def get_data(self):
+        if request.method == 'POST':
+            content = {'modules': Module.objects.all()}
+            content = render_to_string('error_list.html', content)
+            return JsonResponse({'modules': content})
+        else:
+            raise Http404('Post request ERROR!')
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(ModuleList, self).get_context_data(**kwargs)
+    #     context['error_list'] = Module.objects.all()
+    #     context['form'] = self.form
+    #     return context
