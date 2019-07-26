c823e31eb4e60372c908dcce3f3d2ddba9195fde
aykoza <zv.am@yandex.ru>
2019-07-26 17:03:18 +0700

    cruise parsing begining

--- test.py
+++ test.py
@@ -2,20 +2,50 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://www.lueftner-cruises.com/en/river-cruises/cruise.html'
+response = requests.get(url).text
+soup = BeautifulSoup(response)
+# print(soup.prettify())
+
+def get_cruises(html):
+    result = []
+    cruises = [i for i in soup.find_all('li', {'class': "cruise-item"})]
+    for cruise in cruises:
+        result.append({'name': get_cruise_name(cruise),
+                       'days': get_cruise_date(cruise)})
+    return result
 
 
-def get_html(url):
-    response = requests.get(url)
-    return response.text
+def get_cruise_name(cruise):
+    return cruise.find('span', {'class': "cruise-name"}).string
 
-def get_all_links(html):
-    soup = BeautifulSoup(html, 'lxml')
-    tds = soup.find('table', id='currencies-all').find_all('td', class_='currency-name')
-    links = []
-    for td in tds:
-        a = td.find('a', class_='currency-name-container').get('href')
-        link = 'https://coinmarketcap.com' + a
-        links.append(link)
-    return links
 
-print(get_html(url))
+def get_cruise_date(cruise):
+    print(cruise.findAll('span', {'class': "small-btn-font"}))
+    return cruise.findAll('span', {'class': "cruise-from-price"})
+
+print(get_cruises(soup))
+
+
+
+
+
+# print(soup)
+
+# def get_html(url):
+#     response = requests.get(url)
+#     return response.text
+#
+# def get_cruise_name(html):
+#     soup = BeautifulSoup(html)
+#     # print(soup.prettify())
+#     for i in soup.find_all('span', {'class':"cruise-name"}):
+#         print(i)
+#
+# def get_cruise_from_price(html):
+#     soup = BeautifulSoup(html)
+#     soup.prettify()
+#     for i in soup.find_all('span', {'class':"cruise-from-price"}):
+#         print(i)
+#
+# get_cruise_name(get_html(url))
+# get_cruise_from_price(get_html(url))
