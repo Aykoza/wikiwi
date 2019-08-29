9c3d553ea25d3bfb6f22509251c35f697e629512
aykoza <zv.am@yandex.ru>
2019-08-29 08:18:08 +0700

    add requests for form

--- wiki/urls.py
+++ wiki/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', Main.as_view(), name='index'),
     path('modules/', ModuleList.get_data, name='modules'),
     path('forms/', ViewList.get_data, name='forms'),
+    path('form/', ViewList.get_view, name='form'),
     path('views/', ViewList.as_view(), name='views'),
     path('errors/', ErrorList.as_view(), name='errors'),
 ]
--- wiki/views/form.py
+++ wiki/views/form.py
@@ -23,3 +23,14 @@ class ViewList(generic.ListView):
             return JsonResponse({'forms': forms})
         else:
             raise Http404('Post request ERROR!')
+
+    def get_view(request):
+        # print(request.GET['id'])
+        if request.method == 'GET':
+            qs_form = View.objects.filter(id=request.GET['id'])
+            print(qs_form[0])
+            form = render_to_string('form.html', {'form': View.objects.filter(id=request.GET['id'])[0]})
+            # modules = serialize('json', Module.objects.all())
+            return JsonResponse({'form': form})
+        else:
+            raise Http404('Post request ERROR!')
